3e4e8a1d45167b4a7706eb9b211fcbce80d5f2e4
SiddNawale <95672846+SiddNawale@users.noreply.github.com>
2022-12-20 21:04:53 +0530

    Branch21 (#16)
    * added other
    
    * ADDED

---
+++ check.sql
@@ -0,0 +1,28 @@
+WITH basedata as (
+    SELECT
+        distinct obt.COMPANY_ID,
+        obt.REPORTING_DATE,
+        IFF(
+            PRODUCT_CATEGORIZATION_PRODUCT_PLAN in ('-', 'Basic', 'Standard', 'Premium')
+            and PRODUCT_CATEGORIZATION_LICENSE_SERVICE_TYPE in (
+          a      'SaaS',
+                'Maintenance',
+                'On Premise (Subscription)'
+            ),
+            UNITS,
+            0
+        ) AS UNITS,
+        MRR AS MRR
+    FROM
+        ANALYTICS.DBO.GROWTH__OBT obt
+    WHERE
+        METRIC_OBJECT = 'applied_billings'
+        AND PRODUCT_CATEGORIZATION_PRODUCT_LINE = 'Manage'
+        AND PRODUCT_CATEGORIZATION_PRODUCT_PACKAGE = 'Manage'
+        AND PRODUCT_CATEGORIZATION_LICENSE_SERVICE_TYPE in (
+            'SaaS',
+            'Maintenance',
+            'On Premise (Subscription)'
+        )
+        AND MRR <> 0
+)
