933163b71b89bc7657d52198c6febd6366d62f6a
SiddNawale <siddhesh.nawale@connectwise.com>
2022-12-21 21:20:54 +0530

    Resolving pre-commit-hook changes

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
 print("This is an test check please do not bother to interfere ")
-print('Checking again')
-print('Silver')
-print('platinum')
-print('joker')
+print("Checking again")
+print("Silver")
+print("platinum")
+print("joker")
 
-print('eleven and queen added')
-=======
-print('eleve')
- 
+print("eleven and queen added")
+
+print("eleven")
